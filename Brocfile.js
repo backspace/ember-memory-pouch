60c1b4ea64256afef7e18be1ee620bf9c201a933
Buck Doyle <b@chromatin.ca>
2014-12-25 15:37:35 -0500

    Add es5-shim for PhantomJS support

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/es5-shim/es5-shim.js');
 app.import('bower_components/pouchdb/dist/pouchdb.js');
 
 module.exports = app.toTree();
