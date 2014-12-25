78ff0197b21d265ec4d39cbb80ea720fc2fe6b98
Buck Doyle <b@chromatin.ca>
2014-12-25 16:33:27 -0500

    Add non-test failure illustration

---
+++ app/components/pouch-display.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  regularStatus: 'not ready',
+  memoryStatus: 'not ready',
+  
+  initializeDatabases: Ember.on('init', function() {
+    var component = this;
+    new PouchDB('regular').then(function() {
+      component.set('regularStatus', 'success');
+    }, function(error) { 
+      component.set('regularStatus', 'failure: ' + error.stack);
+    });
+
+    new PouchDB('memory', {adapter: 'memory'}).then(function() {
+      component.set('memoryStatus', 'success');
+    }, function(error) { 
+      component.set('memoryStatus', 'failure: ' + error.stack);
+    });
+  })
+});
