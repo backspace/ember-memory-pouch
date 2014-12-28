68bf85d7fcd4ecc5efc74177fd0c6bb932500bf1
Buck Doyle <b@chromatin.ca>
2014-12-27 21:39:06 -0500

    Remove pesky trailing whitespace

--- app/components/pouch-display.js
+++ app/components/pouch-display.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   regularStatus: 'not ready',
   memoryStatus: 'not ready',
-  
+
   initializeDatabases: Ember.on('init', function() {
     var component = this;
     new PouchDB('regular').then(function() {
       component.set('regularStatus', 'success');
-    }, function(error) { 
+    }, function(error) {
       component.set('regularStatus', 'failure: ' + error.stack);
     });
 
     new PouchDB('memory', {adapter: 'memory'}).then(function() {
       component.set('memoryStatus', 'success');
-    }, function(error) { 
+    }, function(error) {
       component.set('memoryStatus', 'failure: ' + error.stack);
     });
   })
